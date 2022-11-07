e4c65aa960886a4f2df0702afb635b4e4600fa65
Smael Nicolas <nico.smael@gmail.com>
2022-11-07 13:28:09 -0300

    import swiper

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,12 @@
+import { SwiperHome } from "../../components/SwiperHome/SwiperHome";
 import "./home.css";
 
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<div className='container--home'>
+			<SwiperHome />
+		</div>
+	);
 };
 
 export default Home;
